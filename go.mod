ced457f27f6d70be4511728c57c772c6ad20c337
Анатолий Елютин <elutin@Nar-Shaddaa.local>
2024-05-17 11:22:42 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module test
+
+go 1.22.2
+
+require (
+	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
+	github.com/charmbracelet/bubbletea v0.22.2-0.20221016150627-cbe309d6241c // indirect
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
+	github.com/charmbracelet/lipgloss v0.6.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/maaslalani/confetty v0.0.0-20221105190856-6c6f1b5b605f // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.13.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/zerefwayne/quizgo v0.0.0-20201002032221-c6068655f835 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.0.0-20221017184919-83659145692c // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/maaslalani/confetty/confetti"
+)
+
+type Question struct {
+	Text     string
+	Options  []string
+	Correct  int
+	Answered int
+}
+
+func runConfetti() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	model := confetti.InitialModel()
+	t := tea.NewProgram(model)
+
+	go t.Run()
+
+	go func(t *tea.Program) {
+		defer wg.Done()
+		for range time.Tick(time.Second) {
+			t.Send(tea.KeyMsg{})
+		}
+	}(t)
+
+	wg.Wait()
+}
+
+func waitInput() {
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+}
+
+func runQuiz() {
+	questions := []Question{
+		{
+			Text:    "Чему будет равно это условие? \n[] == ![];",
+			Options: []string{"True", "False"},
+			Correct: 0,
+		},
+		{
+			Text:    "Какая строка получится в результате этой конкатенации строк? \"foo\" + +\"bar\";",
+			Options: []string{"fooNaN", "foobar"},
+			Correct: 0,
+		},
+		{
+			Text:    "Чему будет равно это условие? NaN === NaN;",
+			Options: []string{"True", "False"},
+			Correct: 1,
+		},
+		{
+			Text:    "Чему будет равно это выражение? 0.1 + 0.2;",
+			Options: []string{"0.3", "0.3000000004"},
+			Correct: 1,
+		},
+		{
+			Text:    "Чему будет равно это условие? Math.min() > Math.max();",
+			Options: []string{"True", "False"},
+			Correct: 0,
+		},
+		{
+			Text:    "Сплит по строке \"\".split(\"\"); возвращает пустой массив []. Как изменить код, чтобы он вернул массив с пустой строкой [\"\"]?",
+			Options: []string{"\"\".split(\" \")", "\" \".split(\"\")"},
+			Correct: 0,
+		},
+	}
+
+	score := 0
+
+	for i, q := range questions {
+		fmt.Println("Вопрос", i+1, ":", q.Text)
+		for j, option := range q.Options {
+			fmt.Println(j+1, ":", option)
+		}
+
+		var answer int
+		fmt.Print("Напиши ответ (1-2): ")
+		fmt.Scanln(&answer)
+
+		if answer == q.Correct+1 {
+			fmt.Println("Верно!")
+			score++
+		} else {
+			fmt.Println("Неверно. Правильный ответ:", q.Options[q.Correct])
+		}
+
+		fmt.Println()
+
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func main() {
+
+	fmt.Println("Дима, привет!")
+	<-time.After(time.Second * 3)
+
+	fmt.Println("У тебя сегодня день рождения?")
+	<-time.After(time.Second * 3)
+
+	fmt.Println("Кхмм...")
+	<-time.After(time.Second * 3)
+
+	fmt.Println("Просто так мы подарок не дадим ⊂(◉‿◉)つ")
+	<-time.After(time.Second * 4)
+
+	fmt.Println("Придется решить несколько задачек по любимому JavaScript!")
+	<-time.After(time.Second * 5)
+
+	fmt.Println("Ты готов? Напиши Да")
+	<-time.After(time.Second * 5)
+
+	waitInput()
+
+	runQuiz()
+
+	runConfetti()
+}
